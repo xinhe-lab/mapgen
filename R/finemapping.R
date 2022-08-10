b75b5afbeebd5fd195562fc2f4c22c5ef28fb841
kevinlkx <kevinlkx@gmail.com>
2022-08-10 12:18:11 -0500

    keep the previous version of run_susie()

--- R/finemapping.R
+++ R/finemapping.R
@@ -31,14 +31,9 @@ prepare_susie_data_with_torus_result <- function(sumstats, torus_prior, torus_fd
 #' @param bigSNP a bigsnpr object attached via bigsnpr::snp_attach()
 #' @param priortype prior type: "torus" or "uniform".
 #' @param L Number of causal signals
-#' @param estimate_residual_variance The default is FALSE,
-#' the residual variance is fixed to 1 or variance of y.
-#' If the in-sample LD matrix is provided,
-#' we recommend setting estimate_residual_variance = TRUE.
 #' @return list of finemapping results; one per LD block
 #' @export
-run_finemapping <- function(sumstats, R, bigSNP, priortype = c('torus', 'uniform'),
-                            L = 1, estimate_residual_variance=FALSE){
+run_finemapping <- function(sumstats, bigSNP, priortype = c('torus', 'uniform'), L = 1){
 
   priortype <- match.arg(priortype)
 
@@ -55,9 +50,7 @@ run_finemapping <- function(sumstats, R, bigSNP, priortype = c('torus', 'uniform
     z <- chunks[i]
     cat(sprintf('Finemapping locus %s...\t', z))
     susie.df <- sumstats[sumstats$locus == z, ]
-    susie_res[[as.character(z)]] <- run_susie(susie.df, R = R, bigSNP = bigSNP,
-                                              L = L, useprior = useprior,
-                                              estimate_residual_variance = estimate_residual_variance)
+    susie_res[[as.character(z)]] <- run_susie(susie.df, bigSNP, z, L, useprior)
     cat(sprintf('%.0f%% completed.\n', length(susie_res)/length(chunks)*100))
   }
 
@@ -66,70 +59,36 @@ run_finemapping <- function(sumstats, R, bigSNP, priortype = c('torus', 'uniform
 }
 
 
-#' @title Run fine-mapping with SuSiE for one LD block
+#' @title Run fine-mapping with SUSIE for one LD block
 #' @param sumstats summary statistics
-#' @param R p x p LD (correlation) matrix
 #' @param bigSNP bigSNP object
 #' @param locus LD block index
 #' @param L Number of causal signals
 #' @param useprior Logical, if TRUE, use the \code{torus_prior} column
 #' in \code{sumstats} as prior.
-#' @param estimate_residual_variance The default is FALSE,
-#' the residual variance is fixed to 1 or variance of y.
-#' If the in-sample LD matrix is provided,
-#' we recommend setting estimate_residual_variance = TRUE.
 #' @return finemapping results for one LD block
 #' @export
-run_susie <- function(sumstats, R, bigSNP, locus, L=1, useprior,
-                      estimate_residual_variance=FALSE){
+run_susie <- function(sumstats, bigSNP, locus, L=1, useprior){
 
   sub.sumstats <- sumstats[sumstats$locus == locus, ]
   if(nrow(sub.sumstats) > 1){
-    if(missing(R) || is.null(R)){
-      if(missing(bigSNP) || is.null(bigSNP)){
-        stop("Please provide R matrix or bigSNP object!")
-      }
-      cat("Computing R from bigSNP genotype matrix...\n")
-      X <- bigSNP$genotypes[ , sub.sumstats$bigSNP_index]
-      X <- scale(X, center = T, scale = T)
-      R <- cor(X)
-    }
+    X <- bigSNP$genotypes[ , sub.sumstats$bigSNP_index]
+    X <- scale(X, center = T, scale = T)
     zhat <- sub.sumstats$zscore
-
+    R <- cov2cor((crossprod(X) + tcrossprod(zhat))/nrow(X))
     if(useprior){
-      prior_weights <- sub.sumstats$torus_prior
-    }else{
-      prior_weights <- NULL
+      res <- suppressWarnings(susieR::susie_rss(z = zhat,
+                                                prior_weights = sub.sumstats$torus_prior,
+                                                R = R,
+                                                L = L,
+                                                verbose = F))
     }
-
-    res <- suppressWarnings(susieR::susie_rss(z = zhat,
-                                              prior_weights = prior_weights,
-                                              R = R,
-                                              L = L,
-                                              estimate_residual_variance = estimate_residual_variance,
-                                              verbose = F))
-    return(res)
-  }
-}
-
-# Run fine-mapping with SuSiE with reference LD matrix
-run_susie_LD <- function(sumstats, R, locus, L=1, useprior,
-                         estimate_residual_variance=FALSE){
-
-  sub.sumstats <- sumstats[sumstats$locus == locus, ]
-  if(nrow(sub.sumstats) > 1){
-    z <- sub.sumstats$zscore
-    if(useprior){
-      prior_weights <- sub.sumstats$torus_prior
-    }else{
-      prior_weights <- NULL
+    else{
+      res <- suppressWarnings(susieR::susie_rss(z = zhat,
+                                                R = R,
+                                                L = L,
+                                                verbose = F))
     }
-    res <- suppressWarnings(susieR::susie_rss(z = z,
-                                              prior_weights = prior_weights,
-                                              R = R,
-                                              L = L,
-                                              estimate_residual_variance = estimate_residual_variance,
-                                              verbose = F))
     return(res)
   }
 }
