56335a048bd53d310c1abb7f1c0164cabc507043
kevinlkx <kevinlkx@gmail.com>
2023-09-27 15:35:08 -0500

    update process_gwas_sumstats function and change gwas to sumstats

--- R/gwas.R
+++ R/gwas.R
@@ -1,10 +1,10 @@
 #' @title Cleans GWAS summary statistics and adds metadata
-#' @description Cleans GWAS summary statistics and adds metadata
+#' @description Cleans GWAS summary statistics and adds metadata, including:
+#' the locus ID of every SNP, as well as its index in the bigSNP object.
 #'
-#' @param sumstats_file file containing raw summary statistics.
-#' Coordinates should be hg19/b37; we current do not support hg38.
+#' @param sumstats A data frame of GWAS summary statistics.
 #' @param chr Name of the chromosome column in summary statistics file.
-#' @param pos Name of the position column (base pair position, in *hg19*!).
+#' @param pos Name of the position column (base pair position).
 #' @param beta Name of beta column (if you have Odds Ratio,
 #' you will need to transform it to log(Odds Ratio)).
 #' @param se Name of the standard error (se) column.
@@ -12,13 +12,13 @@
 #' @param a1 Column name of the association/effect allele.
 #' @param snp Name of the SNP ID (rsID) column.
 #' @param pval Name of the p-value column.
-#' @param bigSNP bigSNP object from \code{bigsnpr}.
-#' @param LD_Blocks Reference LD blocks
+#' @param bigSNP bigSNP object from \code{bigsnpr} containing the reference
+#' genotype panel.
+#' @param LD_Blocks Reference LD blocks.
 #'
-#' @return Cleaned summary statistics + LD block of every SNP,
-#' as well as its index in the reference panel of genotypes
+#' @return Processed GWAS summary statistics.
 #' @export
-process_gwas_sumstats <- function(sumstats_file,
+process_gwas_sumstats <- function(sumstats,
                                   chr = 'chr',
                                   pos = 'pos',
                                   beta = 'beta',
@@ -27,36 +27,34 @@ process_gwas_sumstats <- function(sumstats_file,
                                   a1 = 'a1',
                                   snp = 'snp',
                                   pval = 'pval',
-                                  bigSNP,
-                                  LD_Blocks){
-
-  cat('Loading summary statistics...\n')
-  if(is.character(sumstats_file) & length(sumstats_file) == 1){
-    sumstats <- vroom::vroom(sumstats_file, col_names = TRUE, show_col_types = FALSE)
-  }
+                                  LD_Blocks = NULL,
+                                  bigSNP = NULL){
 
   cat('Cleaning summary statistics...\n')
-  cleaned_sumstats <- clean_sumstats(sumstats,
+  cleaned.sumstats <- clean_sumstats(sumstats,
                                      chr=chr, pos=pos, beta=beta, se=se,
                                      a0=a0, a1=a1, snp=snp, pval=pval)
 
-  cat('Matching to reference panel...\n')
-  cleaned_sumstats <- merge_bigsnp_gwas(cleaned_sumstats, bigSNP = bigSNP)
-
-  cat('Assigning SNPs to LD blocks...\n')
-  if(missing(LD_Blocks)){
-    cat('No LD blocks supplied. Using the included European LD blocks from 1KG....\n')
-    data('Euro_LD_Chunks', package='mapgen')
+  if(is.null(LD_Blocks)){
+    cat('LD_Blocks not provided. Skipped assigning SNPs to LD blocks. \n')
+  }else{
+    cat('Assigning GWAS SNPs to LD blocks...\n')
+    cleaned.sumstats <- assign_locus_snp(cleaned.sumstats, LD_Blocks)
   }
-  cleaned_sumstats <- assign_locus_snp(cleaned.sumstats = cleaned_sumstats,
-                                       ld = LD_Blocks)
 
-  return(cleaned_sumstats)
+  if(is.null(bigSNP)){
+    cat('bigSNP not provided. Skipped matching GWAS with bigSNP reference panel. \n')
+  }else{
+    cat('Matching GWAS with bigSNP reference panel...\n')
+    cleaned.sumstats <- match_gwas_bigsnp(cleaned.sumstats, bigSNP)
+  }
+
+  return(cleaned.sumstats)
 
 }
 
 #' @title Cleans summary statistics
-#' @param sumstats a tibble or data frame containing raw summary statistics.
+#' @param sumstats A data frame of GWAS summary statistics.
 #' @param cols.to.keep columns to keep.
 #' It is required to have 8 columns in the exact order of:
 #' chr, position, beta, se, allele1, allele2, SNP ID (rs), p-value.
@@ -81,7 +79,6 @@ clean_sumstats <- function(sumstats,
                  cols.to.keep[which(!cols.to.keep %in% colnames(sumstats))]))
   }else{
     # Extract relevant columns
-    cat('Rename and extract columns in summary statistics ...\n')
     cleaned.sumstats <- sumstats[, cols.to.keep]
     colnames(cleaned.sumstats) <- c('chr','pos','beta','se','a0','a1','snp','pval')
   }
@@ -92,25 +89,20 @@ clean_sumstats <- function(sumstats,
     cleaned.sumstats$chr <- gsub('chr', '', cleaned.sumstats$chr)
   }
 
-  # drop X, Y chromosomes
-  cat('Drop X,Y chromosomes ...\n')
+  # Remove X, Y chromosomes
   cleaned.sumstats <- cleaned.sumstats[!(cleaned.sumstats$chr %in% c('X','Y')), ]
-  # make chromosomes integers
-  cat('Chromosomes included: ', unique(cleaned.sumstats$chr), '\n')
   cleaned.sumstats$chr <- as.integer(cleaned.sumstats$chr)
 
   # Compute Zscores
-  cat('Compute z-scores ...\n')
   zscore <- cleaned.sumstats$beta/cleaned.sumstats$se
-  cleaned.sumstats['zscore'] <- zscore
+  cleaned.sumstats$zscore <- zscore
   cleaned.sumstats <- cleaned.sumstats[!is.na(zscore),]
 
-  # convert alleles to upper case
+  # Convert alleles to upper case
   cleaned.sumstats$a0 <- toupper(cleaned.sumstats$a0)
   cleaned.sumstats$a1 <- toupper(cleaned.sumstats$a1)
 
   # Keep SNPs only, no indels
-  cat('Remove indels ...\n')
   nucs <- c('A','C','T','G')
   bola1 <- (cleaned.sumstats$a0 %in% nucs)
   bola2 <- (cleaned.sumstats$a1 %in% nucs)
@@ -119,8 +111,7 @@ clean_sumstats <- function(sumstats,
   # Sort by chromosome and position
   cleaned.sumstats <- cleaned.sumstats[order(cleaned.sumstats$chr, cleaned.sumstats$pos), ]
 
-  # drop duplicate SNPs
-  cat('Remove duplicate SNPs ...\n')
+  # Remove duplicate SNPs
   chrpos <- paste0(cleaned.sumstats$chr, '_', cleaned.sumstats$pos)
   cleaned.sumstats <- cleaned.sumstats[!duplicated(chrpos), ]
 
@@ -128,25 +119,23 @@ clean_sumstats <- function(sumstats,
 }
 
 
-#' @title Assigns each SNP to one ld-block
-#' @param cleaned.sumstats Cleaned summary statistics
-#' @param ld LD blocks
-#' @importFrom magrittr %>%
+#' @title Assigns each SNP to one LD block
+#' @param cleaned.sumstats A data frame of GWAS summary statistics.
+#' @param LD_Blocks A data frame of LD blocks
 #' @export
-assign_locus_snp <- function(cleaned.sumstats, ld){
+assign_locus_snp <- function(cleaned.sumstats, LD_Blocks){
 
-  ldRanges <- make_ranges(ld$X1, ld$X2, ld$X3)
-  ldRanges <- plyranges::mutate(ldRanges, locus=ld$X4)
+  ldRanges <- make_ranges(LD_Blocks$X1, LD_Blocks$X2, LD_Blocks$X3)
+  ldRanges <- plyranges::mutate(ldRanges, locus=LD_Blocks$X4)
 
   snpRanges <- make_ranges(seqname = cleaned.sumstats$chr,
                            start = cleaned.sumstats$pos,
                            end = cleaned.sumstats$pos)
-
   snpRanges <- plyranges::mutate(snpRanges, snp=cleaned.sumstats$snp)
 
   snp.ld.overlap <- plyranges::join_overlap_inner(snpRanges, ldRanges)
   snp.ld.block <- tibble::as_tibble(snp.ld.overlap@elementMetadata)
-  snp.ld.block <- snp.ld.block[!duplicated(snp.ld.block$snp), ] # some SNPs are in multiple ld-blocks due to edge of ld blocks
+  snp.ld.block <- snp.ld.block[!duplicated(snp.ld.block$snp), ] # some SNPs are in multiple LD blocks due to edge of LD blocks
   cleaned.annot.sumstats <- dplyr::inner_join(cleaned.sumstats, snp.ld.block, 'snp')
 
   return(cleaned.annot.sumstats)
@@ -154,18 +143,18 @@ assign_locus_snp <- function(cleaned.sumstats, ld){
 
 
 #' @title Assigns SNPs with annotations based on overlap
-#' @param gwas a data frame or tibble of GWAS summary statistics
-#' @param annotations annotation BED files
+#' @param sumstats A data frame of GWAS summary statistics
+#' @param annotations Paths to annotation BED files
 #' @param keep.annot.only Logical. If TRUE, only returns the
 #' snp and annotation columns
 #' @return if \code{keep.annot.only=TRUE}, returns
 #' a data frame or tibble of snp and annotations. Otherwise, returns
 #' a data frame or tibble of GWAS summary statistics and annotations.
 #' @export
-annotate_snps_binary <- function(gwas, annotations, keep.annot.only=TRUE){
+annotate_snps_binary <- function(sumstats, annotations, keep.annot.only=TRUE){
 
-  snpRanges <- make_ranges(gwas$chr, gwas$pos, gwas$pos)
-  snpRanges <- plyranges::mutate(snpRanges, snp=gwas$snp)
+  snpRanges <- make_ranges(sumstats$chr, sumstats$pos, sumstats$pos)
+  snpRanges <- plyranges::mutate(snpRanges, snp=sumstats$snp)
 
   for(f in annotations){
     name <- paste0(basename(f),'_d')
@@ -173,26 +162,26 @@ annotate_snps_binary <- function(gwas, annotations, keep.annot.only=TRUE){
     annot.gr <- rtracklayer::import(f, format='bed')
     snpRangesIn <- IRanges::subsetByOverlaps(snpRanges, annot.gr)
     snpsIn <- unique(snpRangesIn$snp)
-    gwas <- dplyr::mutate(gwas, !!name := ifelse(snp %in% snpsIn,1,0))
+    sumstats <- dplyr::mutate(sumstats, !!name := ifelse(snp %in% snpsIn,1,0))
   }
 
   if(keep.annot.only){
-    gwas <- gwas[,c('snp', paste0(basename(annotations), '_d'))]
+    sumstats <- sumstats[,c('snp', paste0(basename(annotations), '_d'))]
   }
 
-  return(gwas)
+  return(sumstats)
 }
 
 #' @title Annotations for causal SNPs (apply these after fine-mapping!)
-#' @param gwas a data frame or tibble of GWAS summary statistics
-#' @param annotations annotation BED files
+#' @param sumstats A data frame of GWAS summary statistics
+#' @param annotations Paths to annotation BED files
 #' @importFrom magrittr %>%
 #' @export
-annotator_merged <- function(gwas, annotations){
+annotator_merged <- function(sumstats, annotations){
 
-  snpRanges <- make_ranges(gwas$chr, gwas$pos, gwas$pos)
-  snpRanges <- plyranges::mutate(snpRanges, snp=gwas$snp)
-  gwas['annots'] <- ''
+  snpRanges <- make_ranges(sumstats$chr, sumstats$pos, sumstats$pos)
+  snpRanges <- plyranges::mutate(snpRanges, snp=sumstats$snp)
+  sumstats['annots'] <- ''
 
   for(f in annotations){
 
@@ -201,36 +190,40 @@ annotator_merged <- function(gwas, annotations){
     snpsIn <- unique(snpRangesIn$snp)
 
     if(length(snpsIn)>0){
-      curr <- gwas %>% pull(annots)
-      curr <- curr[gwas$snp %in% snpsIn]
+      curr <- sumstats %>% pull(annots)
+      curr <- curr[sumstats$snp %in% snpsIn]
       delims <- rep(';', length(curr))
       delims[which(curr == '')] <- ''
-      gwas[gwas$snp %in% snpsIn,"annots"] <- paste0(curr,delims,gsub(pattern = '.bed',replacement = '', x = basename(f)))
+      sumstats[sumstats$snp %in% snpsIn,"annots"] <- paste0(curr,delims,gsub(pattern = '.bed',replacement = '', x = basename(f)))
     }
   }
-  return(gwas)
+  return(sumstats)
 }
 
-#' @title Match GWAS SNPs with reference panel from the bigSNP object.
-#' @param gwas  GWAS summary statistics
+#' @title Match GWAS with bigSNP reference panel.
+#' @param sumstats  A data frame of GWAS summary statistics
 #' @param bigSNP  bigSNP object
+#' @param strand_flip Whether to try to flip strand? (default is TRUE).
+#' If so, ambiguous alleles A/T and C/G are removed.
+#' @param match.min.prop Minimum proportion of variants in the smallest data
+#' to be matched, otherwise stops with an error. Default: 10%
 #' @importFrom magrittr %>%
 #' @export
-merge_bigsnp_gwas <- function(gwas, bigSNP){
+match_gwas_bigsnp <- function(sumstats, bigSNP, strand_flip = TRUE, match.min.prop = 0.1){
 
   map <- bigSNP$map
   snp_info <- map[,c('chromosome','physical.pos','allele1','allele2')]
   colnames(snp_info) <- c('chr','pos','a0','a1')
 
-  matched.gwas <- tibble::as_tibble(bigsnpr::snp_match(gwas,
-                                                       snp_info,
-                                                       strand_flip = TRUE,
-                                                       match.min.prop = 0.1))
+  matched.sumstats <- tibble::as_tibble(bigsnpr::snp_match(sumstats,
+                                                           snp_info,
+                                                           strand_flip = strand_flip,
+                                                           match.min.prop = match.min.prop))
 
-  matched.gwas <- matched.gwas %>%
+  matched.sumstats <- matched.sumstats %>%
     dplyr::rename(og_index = `_NUM_ID_.ss`) %>%
     dplyr::rename(bigSNP_index = `_NUM_ID_`) %>%
     dplyr::mutate(zscore = beta/se)
 
-  return(matched.gwas)
+  return(matched.sumstats)
 }
