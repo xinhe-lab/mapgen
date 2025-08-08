510650947f3d7e4706c9f84dce5aa6c4cf907cee
kevinlkx <kevinlkx@gmail.com>
2025-08-08 10:59:27 -0500

    add p-value and rename file

--- R/LD_diagnosis.R
+++ R/LD_diagnosis.R
@@ -9,14 +9,20 @@
 #'
 #' @return a data frame of expected z-scores and test statistics
 #' from \code{susieR::kriging_rss}.
+#' @importFrom susieR estimate_s_rss kriging_rss
 #' @export
 LD_diagnosis_susie_rss <- function(z, R, n){
+
   cat('Estimate consistency between the z-scores and reference LD matrix ...\n')
   lambda <- susieR::estimate_s_rss(z = z, R = R, n = n)
   cat('Estimated lambda =', lambda, '\n')
 
+  # Compute expected z-scores based on conditional distribution of z-scores
   cat('Compute expected z-scores based on conditional distribution of z-scores ...\n')
-  condz <- susieR::kriging_rss(z = z, R = R, n = n, s = lambda)
+  kriging_res <- susieR::kriging_rss(z = z, R = R, n = n, s = lambda)
 
-  return(condz)
+  # compute p-values for the significance of z-score difference between observed and estimated values
+  kriging_res$conditional_dist$p_diff <- pchisq(kriging_res$conditional_dist$z_std_diff^2, df = 1, lower.tail=FALSE)
+
+  return(kriging_res)
 }
